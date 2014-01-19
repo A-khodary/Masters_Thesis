35353810062eae8ab134fe4d51ed72f8e871737b
DanielAshley <dashley2@mail.usf.edu>
2014-01-19 15:32:46 -0500

    Test 4 - Non-working

--- equationChecker2/equationChecker2/Form1.h
+++ equationChecker2/equationChecker2/Form1.h
@@ -477,16 +477,27 @@ namespace equationChecker2 {
 
 					double _x, _y, _z;
 
+					//Test 4
+					_z = cos(J2)*cos(J1) + sin(J2) - cos(J2)*sin(J1) - 110*sin(J2);
+					_x = -sin(J2)*cos(J1) + cos(J2) + sin(J2)*sin(J1) - 110*cos(J2);
+					_y = sin(J1) + cos(J1) + 146;
+					this->textBoxUAccX->Text = _x.ToString();
+					this->textBoxUAccY->Text = _y.ToString();
+					this->textBoxUAccZ->Text = _z.ToString();
+/*
+					//Test 3
 					_z = -1*sin(J1)*cos(J2);
 					_x = sin(J1)*sin(J2);
 					_y = cos(J1);
 					this->textBoxUAccX->Text = _x.ToString();
 					this->textBoxUAccY->Text = _y.ToString();
 					this->textBoxUAccZ->Text = _z.ToString();
-					
-/*					
+		
+
+
+					//Test 2
 					_z = cos(J2)*cos(J1) + sin(J2) - cos(J2)*sin(J1);
-					_x = -sin(J2)*cos(J2) + cos(J2) + sin(J2)*sin(J1);
+					_x = -sin(J2)*cos(J1) + cos(J2) + sin(J2)*sin(J1);
 					_y = sin(J1) + cos(J1);
 					this->textBoxUAccX->Text = _x.ToString();
 					this->textBoxUAccY->Text = _y.ToString();
